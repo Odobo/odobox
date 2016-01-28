b9971c8ebc87e25ef4b04a6ee7c691ff868f667f
cristina_ionescu <cristina.ionescu@odobo.com>
2016-01-28 10:46:38 +0100

    Removed Q promises from mongo lib

--- mongo.js
+++ mongo.js
@@ -2,7 +2,6 @@
  * Created by cristinaionescu on 26/01/2016.
  */
 var MongoClient = require("vertx-mongo-js/mongo_client");
-var Q = require('../q/q');
 
 module.exports = MongoClientObj;
 
@@ -21,24 +20,21 @@ function MongoClientObj(host, port, dbName, poolname) {
 };
 
 
-MongoClientObj.prototype.saveObjectToDB = function(object, colName) {
+MongoClientObj.prototype.saveObjectToDB = function(object, colName, cb) {
     var collectionName = colName ? colName : this.collectionName;
-    var deferred = Q.defer();
 
     this.mongoClient.save(collectionName, object, function (id, err) {
         if (!err && !id) {
-            deferred.resolve(object);
+            cb(object);
         } else {
             console.log("ERR " + err);
-            deferred.reject(err);
+            cb(err);
         }
     });
-    return deferred.promise;
 };
 
-MongoClientObj.prototype.getDocumentsFromDB = function(query, colName) {
+MongoClientObj.prototype.getDocumentsFromDB = function(query, colName, cb) {
     var collectionName = colName ? colName : this.collectionName;
-    var deferred = Q.defer();
     var queryResults = [];
 
     this.mongoClient.find(collectionName, query, function (res, res_err) {
@@ -47,14 +43,13 @@ MongoClientObj.prototype.getDocumentsFromDB = function(query, colName) {
                 Array.prototype.forEach.call(res, function(doc) {
                     queryResults.push(doc);
                 });
-                query._id ? deferred.resolve(queryResults[0]) : deferred.resolve(queryResults);
+                query._id ? cb(queryResults[0]) : cb(queryResults);
             } else {
-                deferred.reject({statusCode: 404, message: "ERROR: Doc from '" + collectionName + "' not found: " + query});
+                cb({statusCode: 404, message: "ERROR: Doc from '" + collectionName + "' not found: " + query});
             }
         } else {
             console.error("Error when fetching from '" + collectionName + "' the doc: " + query);
-            deferred.reject({statusCode: 500, message: res_err.message});
+            cb({statusCode: 500, message: res_err.message});
         }
     });
-    return deferred.promise;
 };
